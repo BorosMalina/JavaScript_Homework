88781e633e4571a95937fd45f73c73e548ba2a52
Malina <borosmalina@gmail.com>
2023-10-30 18:43:48 +0200

    Rock Paper Scissors Game complete

--- Homework 2 - RockPaperScissors/app.js
+++ Homework 2 - RockPaperScissors/app.js
@@ -1,13 +1,49 @@
 let choices = ["rock", "paper", "scissors"];
-let randomChoice = Math.floor(Math.random() * 3);
+let randomChoice;
 
 function gamePicks() {
   randomChoice = Math.floor(Math.random() * 3);
   let inputElement = document.getElementById("playerInput");
   let playerChoice = inputElement.value;
 
-  let outputElement = document.getElementById("output");
-  outputElement.innerText = choices[randomChoice];
+  let computerChoice = choices[randomChoice];
 
+  let outputElement = document.getElementById("output");
+  outputElement.innerText = "The computer chose... " + computerChoice + "!";
+
+  if (computerChoice == playerChoice)
+  {
+    document.getElementById("result").innerText = "It's a tie!😐";
+  }
+
+  if (computerChoice == "rock" && playerChoice == "paper")
+  {
+    document.getElementById("result").innerText = "You won!😎";
+  }
+
+  if (computerChoice == "rock" && playerChoice == "scissors")
+  {
+    document.getElementById("result").innerText = "You lost!😢";
+  }
+
+  if (computerChoice == "paper" && playerChoice == "rock")
+  {
+    document.getElementById("result").innerText = "You lost!😢";
+  }
+
+  if (computerChoice == "paper" && playerChoice == "scissors")
+  {
+    document.getElementById("result").innerText = "You won!😎";
+  }
+
+  if (computerChoice == "scissors" && playerChoice == "rock")
+  {
+    document.getElementById("result").innerText = "You won!😎";
+  }
+
+  if (computerChoice == "scissors" && playerChoice == "paper")
+  {
+    document.getElementById("result").innerText = "You lost!😢";
+  }
 }
 
